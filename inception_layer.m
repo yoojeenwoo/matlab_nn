4656e424cfc90115f1dbbb3bc940ca3a68828ca2
yoojeenwoo <eugenewu0@gmail.com>
2018-05-17 11:03:38 -0700

    removed unnecessary repmats

--- inception_layer.m
+++ inception_layer.m
@@ -1,37 +1,31 @@
 function [fm_out] = inception_layer(fm_in, branch1, branch2, branch3, branch4, EPSILON)
 
-fm_out_1 = caffe_conv(fm_in, branch1.weights./repmat(reshape(branch1.batchvar + EPSILON, 1, 1, 1, []), ...
-size(branch1.weights, 1), size(branch1.weights, 2), size(branch1.weights, 3), 1), 1, 0);
+fm_out_1 = caffe_conv(fm_in, branch1.weights./reshape(branch1.batchvar + EPSILON, 1, 1, 1, []), 1, 0);
 fm_out_1 = scale_add_bias(fm_out_1, -branch1.batchmean./(branch1.batchvar + EPSILON) + ...
 branch1.batchbeta);
 fm_out_1 = caffe_relu(fm_out_1);
 
 
-fm_out_2 = caffe_conv(fm_in, branch2.weights_a./repmat(reshape(branch2.batchvar_a + EPSILON, 1, 1, 1, []), ...
-size(branch2.weights_a, 1), size(branch2.weights_a, 2), size(branch2.weights_a, 3), 1), 1, 0);
+fm_out_2 = caffe_conv(fm_in, branch2.weights_a./reshape(branch2.batchvar_a + EPSILON, 1, 1, 1, []), 1, 0);
 fm_out_2= scale_add_bias(fm_out_2, -branch2.batchmean_a./(branch2.batchvar_a + EPSILON) + ...
 branch2.batchbeta_a);
 fm_out_2= caffe_relu(fm_out_2);
-fm_out_2 = caffe_conv(fm_out_2, branch2.weights_b./repmat(reshape(branch2.batchvar_b + EPSILON, 1, 1, 1, []), ...
-size(branch2.weights_b, 1), size(branch2.weights_b, 2), size(branch2.weights_b, 3), 1), 1, 1);
+fm_out_2 = caffe_conv(fm_out_2, branch2.weights_b./reshape(branch2.batchvar_b + EPSILON, 1, 1, 1, []), 1, 1);
 fm_out_2 = scale_add_bias(fm_out_2, -branch2.batchmean_b./(branch2.batchvar_b + EPSILON) + ...
 branch2.batchbeta_b);
 fm_out_2 = caffe_relu(fm_out_2);
 
-fm_out_3 = caffe_conv(fm_in, branch3.weights_a./repmat(reshape(branch3.batchvar_a + EPSILON, 1, 1, 1, []), ...
-size(branch3.weights_a, 1), size(branch3.weights_a, 2), size(branch3.weights_a, 3), 1), 1, 0);
+fm_out_3 = caffe_conv(fm_in, branch3.weights_a./reshape(branch3.batchvar_a + EPSILON, 1, 1, 1, []), 1, 0);
 fm_out_3= scale_add_bias(fm_out_3, -branch3.batchmean_a./(branch3.batchvar_a + EPSILON) + ...
 branch3.batchbeta_a);
 fm_out_3= caffe_relu(fm_out_3);
-fm_out_3 = caffe_conv(fm_out_3, branch3.weights_b./repmat(reshape(branch3.batchvar_b + EPSILON, 1, 1, 1, []), ...
-size(branch3.weights_b, 1), size(branch3.weights_b, 2), size(branch3.weights_b, 3), 1), 1, 1);
+fm_out_3 = caffe_conv(fm_out_3, branch3.weights_b./reshape(branch3.batchvar_b + EPSILON, 1, 1, 1, []), 1, 1);
 fm_out_3 = scale_add_bias(fm_out_3, -branch3.batchmean_b./(branch3.batchvar_b + EPSILON) + ...
 branch3.batchbeta_b);
 fm_out_3 = caffe_relu(fm_out_3);
 
 fm_out_4 = caffe_pool(fm_in, 3, 1, 1);
-fm_out_4 = caffe_conv(fm_out_4, branch4.weights./repmat(reshape(branch4.batchvar + EPSILON, 1, 1, 1, []), ...
-size(branch4.weights, 1), size(branch4.weights, 2), size(branch4.weights, 3), 1), 1, 0);
+fm_out_4 = caffe_conv(fm_out_4, branch4.weights./reshape(branch4.batchvar + EPSILON, 1, 1, 1, []), 1, 0);
 fm_out_4 = scale_add_bias(fm_out_4, -branch4.batchmean./(branch4.batchvar + EPSILON) + ...
 branch4.batchbeta);
 fm_out_4 = caffe_relu(fm_out_4);
